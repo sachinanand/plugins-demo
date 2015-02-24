40383a0500f28a940d827282cb1b46bbafe87353
Sachin <Sachin Anand>
2015-02-24 11:46:12 +0530

    Added local config and sent mail using template

--- grails-app/controllers/com/ig/demo/DummyController.groovy
+++ grails-app/controllers/com/ig/demo/DummyController.groovy
@@ -5,11 +5,19 @@ class DummyController {
     def mailService
     def index() {
 
+        // send a mail basic functionality
         mailService.sendMail {
             to "sachin@intelligrape.com"
             subject "Hello Sachin"
             body 'How are you?'
         }
+    }
 
+    def sendMailFromTemplate(){
+        mailService.sendMail {
+            to "sachin@intelligrape.com"
+            subject "Hello Sachin"
+            html g.render(template:"myMailTemplate")
+        }
     }
 }
---
+++ grails-app/views/dummy/_myMailTemplate.gsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sachin
+  Date: 24/02/15
+  Time: 11:38 AM
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>This is a mail template</title>
+</head>
+
+<body>
+This mail is coming from a mail template!
+</body>
+</html>
